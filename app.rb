334ad3327ad473da12797f2a75e2da4262365929
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-03-19 02:00:34 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,8 +1,40 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get('/') do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get('/rock') do
+  moves = ["rock", "paper", "scissors"]
+
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @result = "tied"
+  elsif @computer_move == "paper"
+    @result = "lost"
+  else
+    @result = "won"
+  end
+
+
+erb(:rock)
+
+end
+
+get('/paper') do
+
+  "
+  "
+end
+
+get('/scissors') do
+
+  "
+  "
+
 end
